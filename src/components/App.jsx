df773fffe5d7234defc8f89cc81614911f3f07cf
d-tysh <39271488+d-tysh@users.noreply.github.com>
2023-08-04 13:38:15 +0300

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,53 @@
-import { Component } from "react";
+import { useReducer } from "react";
 import { Statistics } from "./Statistics/Statistics";
 
-export class App extends Component {
-  state = {
+const addFeedback = (state, action) => {
+  switch(action.type) {
+    case 'good':
+      return {...state, good: state.good + action.payload};
+    case 'neutral':
+      return {...state, neutral: state.neutral + action.payload};
+      case 'bad':
+        return {...state, bad: state.bad + action.payload};
+      default:
+      return;
+  }
+}
+
+export const App = () => {
+  const [state, dispatch] = useReducer(addFeedback, {
     good: 0,
     neutral: 0,
     bad: 0
-  }
+  });
 
-  addFeedback = (value) => {
-    this.setState(prevState => {
-      return {
-        [value]: prevState[value] + 1
-      }
-    })
-  }
+  // const [good, setGood] = useState(0);
+  // const [neutral, setNeutral] = useState(0);
+  // const [bad, setBad] = useState(0);
 
-  render() {
-    return (
-      <div>
-        <Statistics
-          good={this.state.good}
-          neutral={this.state.neutral}
-          bad={this.state.bad}
-          addFeedback={this.addFeedback}/>
-      </div>
-    );
-  }
+  // const addFeedback = (value) => {
+  //   switch (value) {
+  //     case 'good':
+  //       setGood(state => state + 1);
+  //       break;
+  //     case 'neutral':
+  //       setNeutral(state => state + 1);
+  //       break;
+  //     case 'bad':
+  //       setBad(state => state + 1);
+  //       break;
+  //     default:
+  //       return;
+  //   }
+  // }
+
+  return (
+    <div>
+      <Statistics
+        good={state.good}
+        neutral={state.neutral}
+        bad={state.bad}
+        addFeedback={(value) => dispatch({type: value, payload: 1})} />
+    </div>
+  );
 };
